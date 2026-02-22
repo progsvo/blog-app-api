0daca5f69a2dc5c334cb066df6a862f4b2822457
risuvoo <rafiqulislamsuvobd@gmail.com>
2026-02-22 21:38:45 +0600

    ✅ Feat: filtering feature added

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 const createPost = async (req: Request, res: Response) => {
     try {
         const user = req.user;
@@ -25,7 +26,26 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPosts = async (req: Request, res: Response) => {
     try {
-        const result = await postService.getAllPosts();
+        // for search
+        const { search } = req.query;
+        const searchStr = typeof search === "string" ? search : undefined;
+        // tags
+        const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+        // isFeatured (true or false)
+        const isFeatured = req.query.isFeatured
+            ? (req.query.isFeatured as string) === "true"
+                ? true
+                : (req.query.isFeatured as string) === "false"
+                    ? false
+                    : undefined
+            : undefined;
+        // status
+        const status = req.query.status as PostStatus | undefined;
+        // authorId
+        const authorId = req.query.authorId as string | undefined;
+
+        // service call
+        const result = await postService.getAllPosts({ search: searchStr, tags, isFeatured, status, authorId });
         res.status(200).json({
             success: true,
             message: "Posts fetched successfully",
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../../generated/prisma/client";
+import { PostWhereInput } from './../../../generated/prisma/internal/prismaNamespace';
+import { Post, PostStatus } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string) => {
@@ -11,8 +12,76 @@ const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt" | "a
     return result;
 }
 
-const getAllPosts = async () => {
-    const result = await prisma.post.findMany();
+const getAllPosts = async ({
+    search,
+    tags,
+    isFeatured,
+    status,
+    authorId
+}: {
+    search: string | undefined,
+    tags: string[] | []
+    isFeatured: boolean | undefined,
+    status: PostStatus | undefined,
+    authorId: string | undefined
+}) => {
+    const andCondition: PostWhereInput[] = [];
+    if (search) {
+        andCondition.push({
+            OR: [
+                {
+                    title: {
+                        contains: search as string,
+                        mode: "insensitive"
+                    },
+
+                },
+                {
+                    content: {
+                        contains: search as string,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    tags: {
+                        has: search as string
+                    }
+                }
+            ],
+        })
+    }
+
+    if (tags?.length) {
+        andCondition.push({
+            tags: {
+                hasEvery: tags as string[]
+            }
+        })
+    }
+
+    if (typeof isFeatured === 'boolean') {
+        andCondition.push({
+            isFeatured
+        })
+    }
+
+    if (status) {
+        andCondition.push({
+            status
+        })
+    }
+
+    if (authorId) {
+        andCondition.push({
+            authorId
+        })
+    }
+
+    const result = await prisma.post.findMany({
+        where: {
+            AND: andCondition
+        }
+    });
     return result;
 }
 
